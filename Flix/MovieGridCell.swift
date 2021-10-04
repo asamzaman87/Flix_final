35306733c98ce620dbf87a8f10dbfc452ea5f768
asamzaman87 <88610200+asamzaman87@users.noreply.github.com>
2021-10-04 05:47:24 +0500

    Flix final
    Final flix version

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Asam Zaman on 10/3/21.
+//
+
+import UIKit
+import AlamofireImage
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    //@IBOutlet weak var posterView: UIView!
+}
